d6717a66b747dd66e826777e4352077fa7f00f12
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2025-04-20 11:10:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
+
 import sys
 import glob
 import importlib
@@ -10,66 +11,63 @@ from pyrogram import idle
 import logging
 import logging.config
 
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("imdbpy").setLevel(logging.ERROR)
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-logging.getLogger("aiohttp").setLevel(logging.ERROR)
-logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from database.ia_filterdb import Media
-from database.users_chats_db import db
-from info import *
-from utils import temp
+from config import LOG_CHANNEL, ON_HEROKU, CLONE_MODE, PORT
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 from Script import script 
 from datetime import date, datetime 
 import pytz
 from aiohttp import web
-from plugins import web_server
+from TechVJ.server import web_server
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
 
 import asyncio
-import aiohttp
-from pyromod import listen
 from pyrogram import idle
-from TechVJ.bot import TechVJBot
-from TechVJ.util.keepalive import ping_server
+from plugins.clone import restart_bots
+from TechVJ.bot import StreamBot
+from TechVJ.utils.keepalive import ping_server
 from TechVJ.bot.clients import initialize_clients
 
-# Path to plugin files
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
-
-TechVJBot.start()
+StreamBot.start()
 loop = asyncio.get_event_loop()
 
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
 
-# Ping Koyeb self every 1 min to stay alive
-async def keep_alive_ping():
-    while True:
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get("https://mature-phillida-rvasp-file-store-a53f8c09.koyeb.app/") as resp:  # Replace with your real app URL
-                    print(f"Pinged self: {resp.status}")
-        except Exception as e:
-            print(f"Ping error: {e}")
-        await asyncio.sleep(60)
 
 async def start():
     print('\n')
-    print('Initalizing Your Bot')
-    bot_info = await TechVJBot.get_me()
-    TechVJBot.username = bot_info.username
+    print('Initalizing Tech VJ Bot')
+    bot_info = await StreamBot.get_me()
+    StreamBot.username = bot_info.username
     await initialize_clients()
-
     for name in files:
         with open(name) as a:
             patt = Path(a.name)
@@ -81,40 +79,26 @@ async def start():
             spec.loader.exec_module(load)
             sys.modules["plugins." + plugin_name] = load
             print("Tech VJ Imported => " + plugin_name)
-
     if ON_HEROKU:
         asyncio.create_task(ping_server())
-
-    b_users, b_chats = await db.get_banned()
-    temp.BANNED_USERS = b_users
-    temp.BANNED_CHATS = b_chats
-
-    await Media.ensure_indexes()
-    me = await TechVJBot.get_me()
-    temp.ME = me.id
-    temp.U_NAME = me.username
-    temp.B_NAME = me.first_name
-    TechVJBot.username = '@' + me.username
-
-    logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
-    logging.info(LOG_STR)
-    logging.info(script.LOGO)
-
+    me = await StreamBot.get_me()
     tz = pytz.timezone('Asia/Kolkata')
     today = date.today()
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
-    await TechVJBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
-
     app = web.AppRunner(await web_server())
+    await StreamBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
     await app.setup()
     bind_address = "0.0.0.0"
     await web.TCPSite(app, bind_address, PORT).start()
-
-    asyncio.create_task(keep_alive_ping())  # Keeps Koyeb awake
-
+    if CLONE_MODE == True:
+        await restart_bots()
+    print("Bot Started Powered By @VJ_Botz")
     await idle()
 
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
 
 if __name__ == '__main__':
     try:
@@ -122,3 +106,7 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         logging.info('Service Stopped Bye 👋')
 
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
